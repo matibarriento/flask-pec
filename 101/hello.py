489d76a88c4a2a88b70619badedb88d88ed55100
Matias Barriento <elmatibarriento@gmail.com>
2016-09-23 23:34:07 -0300

    RC1

--- 101/hello.py
+++ 101/hello.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    i = 1
     return "<h1>Hello World</h1>"
 
 
 if __name__ != "main":
-    app.run("0.0.0.0", 5000, True)
+    app.run("0.0.0.0", 5000, debug=True)
